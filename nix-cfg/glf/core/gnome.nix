b9b608ee721a13d78d63d1ca123a768f1c255793
CORAAL <coraal@noreply.world>
2024-11-13 22:54:50 +0100

    gnome -> rice, config module dash-to-dock

--- nix-cfg/glf/core/gnome.nix
+++ nix-cfg/glf/core/gnome.nix
@@ -1,22 +1,29 @@
 # Documentation
 # https://wiki.nixos.org/wiki/GNOME
 
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
+  environment = {
+    systemPackages = with pkgs; [
+      gnomeExtensions.caffeine
+      gnomeExtensions.appindicator
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.gsconnect
+      gnome.gnome-software
+      gnome.adwaita-icon-theme
+      gnome.gnome-tweaks
+      vlc
+      graphite-gtk-theme
+      tela-circle-icon-theme
+      adw-gtk3
+    ];
+  };
 
-  environment.systemPackages = with pkgs; [
-    gnomeExtensions.caffeine
-    gnomeExtensions.appindicator
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.gsconnect
-    gnome.gnome-software
-    gnome.adwaita-icon-theme
-    gnome.gnome-tweaks
-    vlc
-  ];
-
-  services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
+  services = {
+    udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
+    xserver.excludePackages = [ pkgs.xterm ];
+  };
 
   programs.dconf = {
     enable = true;
@@ -25,6 +32,7 @@
 	settings = {
 	  "org/gnome/desktop/wm/preferences" = {
 	    button-layout = "appmenu:minimize,maximize,close";
+	    theme = "adw-gtk3";
 	    # focus-mode = "click";
 	    # visual-bell = false;
 	  };
@@ -33,7 +41,9 @@
 	    tap-to-click = true;
 	    two-finger-scrolling-enabled = true;
 	  };
-	  "org/gnome/desktop/peripherals/keyboard" = { numlock-state = true;  };
+	  "org/gnome/desktop/peripherals/keyboard" = {
+	    numlock-state = true;
+	  };
 	  "org/gnome/shell" = {
 	    disable-user-extensions = false;
 	    enabled-extensions = [
@@ -42,6 +52,18 @@
               "appindicatorsupport@rgcjonas.gmail.com"
               "dash-to-dock@micxgx.gmail.com"
 	    ];
+	  };
+	  "org/gnome/shell/extensions/dash-to-dock" = {
+	    click-action="minimize-or-overview";
+	    dock-position="BOTTOM";
+	    isolate-monitor=false;
+	    multi-monitor=true;
+	    show-mounts-network=true;
+	  };
+    		  "org/gnome/mutter" = {
+	    check-alive-timeout = lib.gvariant.mkUint32 30000;
+	    dynamic-workspaces = true;
+	    edge-tiling = true;
 	  };
 	};
       }

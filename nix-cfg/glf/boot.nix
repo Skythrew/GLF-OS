16aab41e370547c1ad307e629a3ec5a07d408880
CORAAL <coraal@noreply.world>
2024-12-11 12:33:05 +0100

    remove custom vm.max_map_count value because default value is enough

--- nix-cfg/glf/boot.nix
+++ nix-cfg/glf/boot.nix
@@ -9,7 +9,6 @@
     kernelPackages = pkgs.linuxPackages_zen;
     # kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
-    kernel.sysctl = { "vm.max_map_count" = 2147483642; };
     kernelParams = if builtins.elem "kvm-amd" config.boot.kernelModules then [ "amd_pstate=active" ] else [ ];
   };
 }

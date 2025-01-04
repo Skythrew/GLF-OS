3a864e4e74f7a63565a07fd448092ac6e109cadd
Darkone Linux <darkone@darkone.yt>
2025-01-04 14:01:52 -0300

    Optimised plugins, added udev and system-config-printer

--- nix-cfg/glf/imprimante.nix
+++ nix-cfg/glf/imprimante.nix
@@ -1,26 +1,37 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   all-users = builtins.attrNames config.users.users;
   normal-users = builtins.filter (user: config.users.users.${user}.isNormalUser == true) all-users;
 in
 {
+  nixpkgs.config.allowUnfree = lib.mkForce true;
+
   # Configure printer
   services.printing = {
     enable = true;
     startWhenNeeded = true;
     drivers = with pkgs; [
+      brgenml1cupswrapper
       brgenml1lpr
       brlaser
       cnijfilter2
       epkowa
       gutenprint
+      gutenprintBin
       hplip
       hplipWithPlugin
       samsung-unified-linux-driver
-      sane-airscan
       splix
     ];
   };
+
+  # To install printers manually
+  programs.system-config-printer.enable = true;
 
   # Enable autodiscovery
   services.avahi = {
@@ -32,8 +43,17 @@ in
   # systemd.services.cups-browsed.enable = false;
   hardware.sane = {
     enable = true;
-    extraBackends = with pkgs; [ sane-airscan epkowa ];
+    extraBackends = with pkgs; [
+      hplipWithPlugin
+      sane-airscan
+      epkowa
+      utsushi
+    ];
   };
+  services.udev.packages = with pkgs; [
+    sane-airscan
+    utsushi
+  ];
 
   # add all users to group scanner and lp
   users.groups.scanner = {

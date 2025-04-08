e0be3995721f057dbf5d0bbea873bd174549f761
Skythrew <mael.guerin@murena.io>
2025-04-08 18:42:06 +0200

    feat: add `nix-disk-manager` package

---
+++ pkgs/nix-disk-manager/default.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  fetchFromGitHub,
+  wrapGAppsHook4,
+  meson,
+  ninja,
+  pkg-config,
+  glib,
+  glib-networking,
+  desktop-file-utils,
+  gettext,
+  librsvg,
+  blueprint-compiler,
+  python3Packages,
+  sassc,
+  appstream-glib,
+  libadwaita,
+  gtk4,
+  libportal,
+  libportal-gtk4,
+  libsoup_3,
+  polkit,
+  gobject-introspection,
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "nix-disk-manager";
+  version = "1.2.0";
+
+  src = fetchFromGitHub {
+    owner = "Gaming-Linux-FR";
+    repo = "nix-disk-manager";
+    rev = "1.2.0";
+    sha256 = "sha256-4UG9NwdRxtW3NJ4xjqcbpwLaz0dyq1AeXa6XBUAPQXY=";
+  };
+
+  format = "other";
+  dontWrapGApps = true;
+
+  nativeBuildInputs = [
+    appstream-glib
+    blueprint-compiler
+    desktop-file-utils
+    gettext
+    glib
+    gobject-introspection
+    meson
+    ninja
+    wrapGAppsHook4
+    pkg-config
+  ];
+
+  buildInputs = [
+    libadwaita
+    librsvg
+    polkit.bin
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    pygobject3
+  ];
+
+  preFixup = ''
+    makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
+  '';
+
+  meta = with lib; {
+    description = "A simple GUI to manage disks on NixOS";
+    license = licenses.gpl3Plus;
+  };
+}

677162c1524588bd9a3882d6e95462dfe2f35f1f
Skythrew <mael.guerin@murena.io>
2025-04-10 13:52:36 +0200

    feat: add light logo

--- pkgs/glfos-branding/default.nix
+++ pkgs/glfos-branding/default.nix
@@ -19,6 +19,7 @@ stdenvNoCC.mkDerivation rec {
     for SIZE in 16 32 48 64 128 256; do
       mkdir -p $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems
       cp $src/logo/logo-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo.png
+      cp $src/logo/logo_light-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo-light.png
     done
   '';
   

c5bdc4d4b8ab880882af4073aed797bd76b75b90
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-04-11 09:48:41 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,49 +1,129 @@
-# flake.nix (CORRIGÉ - Pour iso-cfg - Version "via GitHub" - Révisé)
 {
-  description = "GLF-OS ISO Configuration - Installer Evaluation Flake";
+  description = "GLF-OS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    glf.url = "github:Gaming-Linux-FR/GLF-OS/stable"; # Référence le flake racine
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
-      nixpkgs,
-      nixpkgs-unstable,
-      glf,
       self,
+      nixpkgs,
+      utils,
       ...
-    }: 
-
+    }:
     let
-      system = "x86_64-linux"; 
-
-      # Configuration pour le nixpkgs stable (sera le 'pkgs' par défaut)
-      pkgsStable = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
+      system = "x86_64-linux";
+      nixpkgsConfig = {
+        allowUnfree = true;
+      };
+      nixosModules = {
+        default = import ./modules/default;
       };
 
-      # Configuration pour le nixpkgs unstable (sera passé en argument spécial)
-      pkgsUnstable = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-      };
+      baseModules = [
+        nixosModules.default
+        { nixpkgs.config = nixpkgsConfig; }
+      ];
+
     in
     {
-      nixosConfigurations."GLF-OS" = nixpkgs.lib.nixosSystem {
-        inherit system; # Maintenant 'system' est défini
-        pkgs = pkgsStable; 
-        modules = [
-          ./configuration.nix 
-          glf.nixosModules.default 
-        ];
+      iso = self.nixosConfigurations."glf-installer".config.system.build.isoImage;
 
-        specialArgs = {
-          pkgs-unstable = pkgsUnstable; 
+      nixosConfigurations = {
+        # Configuration pour l'ISO d'installation avec Calamares
+        "glf-installer" = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = baseModules ++ [
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+            ./iso-cfg/configuration.nix
+            {
+              nixpkgs.overlays = [
+                (_self: super: {
+                  calamares-nixos-extensions = super.calamares-nixos-extensions.overrideAttrs (_oldAttrs: {
+                    postInstall = ''
+                      cp ${./patches/calamares-nixos-extensions/modules/nixos/main.py}                   $out/lib/calamares/modules/nixos/main.py
+                      cp -r ${./patches/calamares-nixos-extensions/config/settings.conf}                 $out/share/calamares/settings.conf
+                      cp -r ${./patches/calamares-nixos-extensions/config/modules/packagechooser.conf}   $out/share/calamares/modules/packagechooser.conf
+                      cp -r ${./patches/calamares-nixos-extensions/branding/nixos/show.qml}        $out/share/calamares/branding/nixos/show.qml
+                      cp -r ${./patches/calamares-nixos-extensions/branding/nixos/white.png}       $out/share/calamares/branding/nixos/white.png
+                      cp -r ${./patches/calamares-nixos-extensions/branding/nixos/base.png}        $out/share/calamares/branding/nixos/base.png
+                      cp -r ${./patches/calamares-nixos-extensions/branding/nixos/fast.png}        $out/share/calamares/branding/nixos/fast.png
+                      cp -r ${./patches/calamares-nixos-extensions/branding/nixos/gaming.png}      $out/share/calamares/branding/nixos/gaming.png
+                      cp -r ${./patches/calamares-nixos-extensions/branding/nixos/logo-glf-os.svg} $out/share/calamares/branding/nixos/logo-glf-os.svg
+                      cp -r ${./patches/calamares-nixos-extensions/branding/nixos/branding.desc}   $out/share/calamares/branding/nixos/branding.desc
+                    '';
+                  });
+                })
+              ];
+            }
+            (
+              { config, ... }:
+              {
+                isoImage = {
+                  volumeID = "GLF-OS-ALPHA-OMNISLASH_3"; #Nom à changer en fonction de la nomination GLF OS
+                  includeSystemBuildDependencies = false;
+                  storeContents = [ config.system.build.toplevel ];
+                  squashfsCompression = "zstd -Xcompression-level 22";
+                  efiSplashImage = ./assets/boot/glf-efi.png;
+                  contents = [
+                    {
+                      source = ./iso-cfg;
+                      target = "/iso-cfg";
+                    }
+                  ];
+                };
+              }
+            )
+          ];
+        };
+
+        # Configuration de test utilisateur simulée
+        "user-test" = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = baseModules ++ [
+            {
+              boot.loader.grub = {
+                enable = true;
+                device = "/dev/sda";
+                useOSProber = true;
+              };
+
+              fileSystems."/" = {
+                device = "/dev/sda1";
+                fsType = "ext4";
+              };
+            }
+          ];
         };
       };
-    };
+
+      inherit nixosModules;
+    }
+    // utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = nixpkgsConfig;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.ruby
+            pkgs.bundler
+          ];
+          shellHook = ''
+            cd docs || exit 1
+            echo "Running bundle install and starting Jekyll server..."
+            bundle config set path 'vendor/bundle'
+            bundle install
+            bundle exec jekyll serve
+          '';
+        };
+      }
+    );
 }

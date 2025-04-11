a0ecbf68ce1400cc3a341a257b35501ffe859004
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-04-11 14:44:27 +0200

    ...... Grub Splash not compliant with attendees

--- modules/default/boot.nix
+++ modules/default/boot.nix
@@ -19,7 +19,7 @@ in
 
   config = lib.mkIf config.glf.boot.enable {
 
-    boot.loader.grub.splashImage = ../../assets/boot/glf-bootloader.png;
+   # boot.loader.grub.splashImage = ../../assets/boot/glf-bootloader.png;
 
     boot = {
       tmp.cleanOnBoot = true;

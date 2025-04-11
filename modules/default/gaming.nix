99363567868a9bc5aee01e2a4979d08f58cfb29d
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-04-11 09:29:30 +0200

    Update gaming.nix

--- modules/default/gaming.nix
+++ modules/default/gaming.nix
@@ -1,60 +1,46 @@
+{ config, pkgs, pkgs-unstable, ... }: # Accepter pkgs-unstable
+let
+  system = "x86_64-linux";
+in
 {
-  lib,
-  config,
-  pkgs,
-  ...
-}:
+  environment.systemPackages = with pkgs-unstable; [ # Utiliser pkgs-unstable
+    heroic
+    lutris
+    mangohud
+    wineWowPackages.staging
+    winetricks
+  ];
 
-{
-
-  options.glf.gaming.enable = lib.mkOption {
-    description = "Enable GLF Gaming configurations";
-    type = lib.types.bool;
-    default = true;
-  };
-
-  config = lib.mkIf config.glf.gaming.enable {
-
-    environment.systemPackages = with pkgs; [
-      heroic
-      lutris
-      vulkan-tools
-      mangohud
-      wineWowPackages.staging
-      winetricks
-    ];
-
-    environment.sessionVariables = {
-      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-MANGOHUD_CONFIG = ''control=mangohud,legacy_layout=0,horizontal,background_alpha=0,gpu_stats,gpu_power,cpu_stats,ram,vram,fps,fps_metrics=AVG,0.001,font_scale=1.05'';
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    MANGOHUD_CONFIG = ''control=mangohud,legacy_layout=0,horizontal,background_alpha=0,gpu_stats,gpu_power,cpu_stats,ram,vram,fps,fps_metrics=AVG,0.001,font_scale=1.05'';
 };
 
-    services.udev.extraRules = ''
-      # USB
-      ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-      ATTRS{name}=="Sony Interactive Entertainment DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-      # Bluetooth
-      ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-      ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-    '';
 
-    hardware.steam-hardware.enable = true;
-    hardware.xpadneo.enable = true;
-    programs.steam.gamescopeSession.enable = true;
+  services.udev.extraRules = ''
+    # USB
+    ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    ATTRS{name}=="Sony Interactive Entertainment DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    # Bluetooth
+    ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+    ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+  '';
 
-    programs.steam = {
-      enable = true;
-      package = pkgs.steam.override {
-        extraEnv = {
-          MANGOHUD = true;
-          OBS_VKCAPTURE = true;
-        };
+  hardware.steam-hardware.enable = true;
+  hardware.xpadneo.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam.override {
+      extraEnv = {
+        MANGOHUD = true;
+        OBS_VKCAPTURE = true;
       };
-      remotePlay.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      extraCompatPackages = with pkgs; [ proton-ge-bin ];
     };
-
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 
 }

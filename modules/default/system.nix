4a7a76342bfea6df6ab46b7c63a381a0f58e6916
Cammi <118620964+camini@users.noreply.github.com>
2025-04-14 19:54:22 +0200

    Update system.nix
    Add garbage collector

--- modules/default/system.nix
+++ modules/default/system.nix
@@ -54,8 +54,11 @@
       settings = {
         auto-optimise-store = true;
       };
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 10d";
+      };
     };
-
   };
-
 }
